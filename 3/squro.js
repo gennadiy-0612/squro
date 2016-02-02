7fad0036cc6d814c76482f23e9d597b0764e1136
mini <intval@i.ua>
2016-02-02 15:27:01 +0200

    complete

--- 3/squro.js
+++ 3/squro.js
@@ -6,32 +6,32 @@ var tree;
 tree = {
     items: {
         span: {
-            DragDrop: document.getElementsByTagName('span'),
+            dragaElemets: document.getElementsByTagName('span'),
             resetter: function () {
-                for (var i = 0; i < this.DragDrop.length; i++) {
-                    this.DragDrop[i].parentNode.setAttribute('draggable', 'false');
-                    this.DragDrop[i].addEventListener('dblclick', tree.dblclick.getTarget, false);
-                    this.DragDrop[i].parentNode.addEventListener('mouseenter', tree.items.mouse.enter.do_it, false);
-                    this.DragDrop[i].parentNode.addEventListener('mouseleave', tree.items.mouse.leave.do_it, false);
-                    if (this.DragDrop[i].parentNode.parentNode.parentNode.tagName === 'BODY') {
-                        this.DragDrop[i].parentNode.addEventListener('dragstart', tree.items.drag.start, false);
-                        this.DragDrop[i].parentNode.addEventListener('dragenter', tree.items.drag.over, false);//For IE 9<
-                        this.DragDrop[i].parentNode.addEventListener('dragover', tree.items.drag.over, false);
-                        this.DragDrop[i].parentNode.addEventListener('drop', tree.items.drag.drop, false);
-                        this.DragDrop[i].parentNode.addEventListener('contextmenu', tree.contMenu.insert, false);
+                for (var i = 0; i < this.dragaElemets.length; i++) {
+                    this.dragaElemets[i].parentNode.setAttribute('draggable', 'false');
+                    this.dragaElemets[i].addEventListener('dblclick', tree.dblclick.getTarget, false);
+                    this.dragaElemets[i].parentNode.addEventListener('mouseenter', tree.items.mouse.enter.doIt, false);
+                    this.dragaElemets[i].parentNode.addEventListener('mouseleave', tree.items.mouse.leave.doIt, false);
+                    if (this.dragaElemets[i].parentNode.parentNode.parentNode.tagName === 'BODY') {
+                        this.dragaElemets[i].parentNode.addEventListener('dragstart', tree.items.drag.start, false);
+                        this.dragaElemets[i].parentNode.addEventListener('dragenter', tree.items.drag.over, false);//For IE 9<
+                        this.dragaElemets[i].parentNode.addEventListener('dragover', tree.items.drag.over, false);
+                        this.dragaElemets[i].parentNode.addEventListener('drop', tree.items.drag.drop, false);
+                        this.dragaElemets[i].parentNode.addEventListener('contextmenu', tree.contMenu.insert, false);
                     }
                 }
             }
         },
         clot: {
             button: document.getElementsByTagName('div'),
-            do_it: function () {
+            doIt: function () {
                 for (var i = 0; i < this.button.length; i++) {
-                    this.button[i].addEventListener('click', tree.items.do_it, false);
+                    this.button[i].addEventListener('click', tree.items.doIt, false);
                 }
             }
         },
-        do_it: function (event) {
+        doIt: function (event) {
             event.stopPropagation();
             if (event.target.textContent === '+') {
                 event.target.parentNode.setAttribute('class', '');
@@ -47,13 +47,13 @@ tree = {
         mouse: {
             enter: {
                 drag: 'true',
-                do_it: function (event) {
+                doIt: function (event) {
                     event.target.setAttribute('draggable', 'true');
                 }
             },
             leave: {
                 drag: 'false',
-                do_it: function (event) {
+                doIt: function (event) {
                     event.target.setAttribute('draggable', 'false');
                 }
             }
@@ -77,15 +77,15 @@ tree = {
                 tree.items.drag.this_insert_before = event.target.parentNode;
                 tree.items.drag.clot = tree.items.drag.this_insert_before.parentNode.insertBefore(tree.items.drag.this_insert, tree.items.drag.this_insert_before);
                 tree.items.drag.this_insert_old.parentNode.removeChild(tree.items.drag.this_insert_old);
-                tree.items.drag.clot.children[0].addEventListener('click', tree.items.do_it, false);
-                tree.items.span.DragDrop = tree.items.drag.clot.getElementsByTagName('span');
+                tree.items.drag.clot.children[0].addEventListener('click', tree.items.doIt, false);
+                tree.items.span.dragaElemets = tree.items.drag.clot.getElementsByTagName('span');
                 tree.items.span.resetter();
             }
         }
     },
     load: function () {
         tree.items.span.resetter.call(tree.items.span);
-        tree.items.clot.do_it.call(tree.items.clot);
+        tree.items.clot.doIt.call(tree.items.clot);
     },
     contMenu: {
         box: {
@@ -125,32 +125,14 @@ tree = {
                 tree.contMenu.box.make.call(tree.contMenu.box);
 
                 tree.contMenu.Item.parents = tree.contMenu.box.el;
-                tree.contMenu.Item.ID = 'pre';
-                tree.contMenu.Item.evListName = tree.contMenu.addList;
-                tree.contMenu.Item.make.call(tree.contMenu.Item);
-                tree.contMenu.Item.childs.textContent = '<';
-                tree.contMenu.Item.el.pre.display = 'none';
-
                 tree.contMenu.Item.ID = 'list';
                 tree.contMenu.Item.evListName = tree.contMenu.addList;
                 tree.contMenu.Item.make.call(tree.contMenu.Item);
-                tree.contMenu.Item.el.list.textContent = tree.contMenu.Item.el.list.textAfterDel = '=';
-                tree.contMenu.Item.el.list.display = 'block';
 
                 tree.contMenu.Item.ID = 'close';
-                tree.contMenu.Item.evListName = tree.contMenu.close;
+                tree.contMenu.Item.evListName = tree.contMenu.closeContMenu;
                 tree.contMenu.Item.make.call(tree.contMenu.Item);
                 tree.contMenu.Item.childs.textContent = 'X';
-                tree.contMenu.Item.el.close.delInfo = {
-                    papa: tree.contMenu.Item.el.close.parentNode.parentNode,
-                    elem: tree.contMenu.Item.el.close.parentNode
-                };
-
-                tree.contMenu.Item.ID = 'next';
-                tree.contMenu.Item.evListName = tree.contMenu.addList;
-                tree.contMenu.Item.make.call(tree.contMenu.Item);
-                tree.contMenu.Item.childs.textContent = '>';
-                tree.contMenu.Item.el.next.display = 'none';
 
                 tree.contMenu.Item.ID = 'clone';
                 tree.contMenu.Item.evListName = tree.contMenu.clone;
@@ -160,27 +142,19 @@ tree = {
                 tree.contMenu.Item.evListName = tree.contMenu.delete;
                 tree.contMenu.Item.make.call(tree.contMenu.Item);
 
-                tree.contMenu.Item.ID = 'edit';
-                tree.contMenu.Item.evListName = tree.dblclick.getTarget;
-                tree.contMenu.Item.make.call(tree.contMenu.Item);
-
                 tree.contMenu.Item.ID = 'save';
                 tree.contMenu.Item.evListName = tree.contMenu.save;
                 tree.contMenu.Item.make.call(tree.contMenu.Item);
-                tree.contMenu.Item.el.save.papaForDelete = tree.contMenu.Item.el.save.parentNode.parentNode;
-                tree.contMenu.Item.el.save.elemForDelete = tree.contMenu.Item.el.save.parentNode;
             }
             else return;
         },
-        navy: [],
-        here: '',
-        close: function (event) {
-            console.log(event.target.parentNode.parentNode);
-            event.target.parentNode.parentNode.parentNode.removeChild(event.target.parentNode.parentNode);
+        closeContMenu: function (event) {
             if (tree.items.span) tree.items.span.removeAttribute('class');
+            document.body.removeChild(event.target.parentNode);
         },
-        show: function () {
-            console.log(this);
+        closeHistory: function (event) {
+            if (tree.items.span) tree.items.span.removeAttribute('class');
+            document.body.removeChild(event.target.parentNode.parentNode);
         },
         delete: function () {
             tree.items.span.parentNode.parentNode.removeChild(tree.items.span.parentNode);
@@ -197,36 +171,39 @@ tree = {
             tree.AJAX.add.call(tree.AJAX);
             this.parentNode.parentNode.removeChild(this.parentNode);
         },
+        addItem: function () {
+            tree.AJAX.parametrs = 'removeitem=' + this.previousElementSibling.previousElementSibling.textContent;
+            tree.AJAX.content = document.getElementsByClassName('node')[0];
+            tree.AJAX.add.call(tree.AJAX);
+            this.parentNode.cloneNode(true);
+            tree.contMenu.save();
+            this.parentNode.parentNode.insertBefore(
+                this.parentNode.cloneNode(true),
+                this.parentNode);
+            this.parentNode.previousSibling.children[1].addEventListener('click', tree.contMenu.moveHistory.make, false);
+            this.parentNode.previousSibling.children[1].textContent = 'Just added';
+            this.parentNode.previousSibling.children[2].addEventListener('click', tree.contMenu.removeItem, false);
+            this.parentNode.previousSibling.children[3].addEventListener('click', tree.contMenu.addItem, false);
+        },
         addList: function (event) {
             tree.AJAX.parametrs = 'history=';
             tree.AJAX.store = document.body.appendChild(document.createElement('div'));
-            tree.AJAX.styler = event.target.display;
-            tree.AJAX.store.papaForDelete = tree.AJAX.store.parentNode;
-            tree.AJAX.store.elemForDelete = tree.AJAX.store;
             tree.AJAX.store.setAttribute('class', 'listcontent');
             tree.AJAX.content = tree.AJAX.store;
-            //tree.contMenu.Item.el.list.removeEventListener('click', tree.contMenu.addList, false);
             tree.AJAX.add.call(tree.AJAX);
-            tree.AJAX.store.setAttribute('style', 'display:' + tree.AJAX.styler + ';');
             this.removeEventListener('click', tree.contMenu.addList, false);
             this.addEventListener('click', tree.contMenu.addList, false);
-            console.log(this)
         },
         historyPrep: function () {
             for (var i = 1; i < this.length - 1; i++) {
-                tree.contMenu.navy[i - 1] = this[i].children[0].textContent;
-                tree.contMenu.moveHistory.ID = tree.contMenu.navy[i - 1];
                 this[i].children[1].addEventListener('click', tree.contMenu.moveHistory.make, false);
                 this[i].children[2].addEventListener('click', tree.contMenu.removeItem, false);
-                this[i].children[3].addEventListener('click', tree.contMenu.save, false);
+                this[i].children[3].addEventListener('click', tree.contMenu.addItem, false);
             }
-            this[0].addEventListener('click', tree.contMenu.close, false);
-            this[0].papaForDelete = this[0].parentNode.parentNode.parentNode;
-            this[0].elemForDelete = this[0].parentNode.parentNode;
+            this[0].addEventListener('click', tree.contMenu.closeHistory, false);
             this[0].ev = {};
             this[0].ev.list = tree.contMenu.addList;
             this[0].ev.elem = tree.contMenu.Item.el.list;
-            //tree.contMenu.Item.el.list.parentNode.setAttribute('style', 'opacity:0;');
             this[this.length - 1].children[0].addEventListener('click', tree.contMenu.Opacity.setting, false);
             this[this.length - 1].children[1].addEventListener('click', tree.contMenu.Opacity.setting, false);
         },
@@ -240,18 +217,16 @@ tree = {
         moveHistory: {
             ID: '',
             make: function () {
-                tree.contMenu.here = this.previousElementSibling.textContent;
                 tree.AJAX.parametrs = 'id=' + this.previousElementSibling.textContent;
                 tree.AJAX.content = document.getElementsByClassName('node')[0];
                 tree.AJAX.add.call(tree.AJAX);
             }
         },
-        save: function () {
+        save: function (event) {
             tree.items.span.removeAttribute('class');
             tree.AJAX.parametrs = 'insert=';
             tree.AJAX.content = document.getElementsByClassName('node')[0].innerHTML;
             tree.AJAX.moveHistory.call(tree.AJAX);
-            console.log(this.parentNode.parentNode.parentNode.parentNode);
         }
     },
     AJAX: {
